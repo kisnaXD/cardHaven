d0f835219dc059b1626d254ed85d230e17ebcd67
kisnaXD <krishnagera93@gmail.com>
2024-12-25 00:07:38 +0530

    First Commit Navbar made

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        "pixelify": ["Pixelify Sans", "sans-serif"],
+        "Inter": ["Inter"],
+        "Poppins": ["Poppins", "serif"],
+        "Metrophobic": ["Metrophobic", "serif"],
+        "Montserrat": ["Montserrat", 'serif'],
+      }
+    },
+  },
+  plugins: [],
+}
